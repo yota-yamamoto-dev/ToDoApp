b0fb9f6806a4443d136e2ce3628cedccacb0c824
Yota Yamamoto <yota.06300630@gmail.com>
2026-07-20 16:46:28 +0900

    step03 タスク一覧機能実装

--- src/ToDo/Main.java
+++ src/ToDo/Main.java
@@ -1,35 +1,50 @@
 package ToDo;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //メニュー表示
-        System.out.println("===== ToDoアプリ =====");
-        System.out.println("1. タスク追加");
-        System.out.println("2. タスク一覧");
-        System.out.println("3. タスク削除");
-        System.out.println("0. 終了");
-        System.out.println();
 
-        //入力番号取得
+        //データ
+        ArrayList<String> todoList = new ArrayList<>();
+        //スキャナー
         Scanner sc = new Scanner(System.in);
-        System.out.print("番号を入力してください：");
-        int num = sc.nextInt();
 
-        //選択機能表示
-        switch (num){
-            case 1 :
-                System.out.print("タスク名を入力してください：");
-                String task = sc.next();
-                System.out.println("「" + task + "」を追加しました。");
-            break;
-            case 2 : System.out.println("タスク一覧を選択しました。");
-            break;
-            case 3 : System.out.println("タスク削除を選択しました。");
-            break;
-            case 0 : System.out.println("アプリを終了します。");
-            break;
+        while (true) {
+            //メニュー表示
+            System.out.println("===== ToDoアプリ =====");
+            System.out.println("1. タスク追加");
+            System.out.println("2. タスク一覧");
+            System.out.println("3. タスク削除");
+            System.out.println("0. 終了");
+            System.out.println();
+
+            //入力番号取得
+            System.out.print("番号を入力してください：");
+            int num = sc.nextInt();
+
+            //選択機能表示
+            switch (num) {
+                case 1:
+                    System.out.print("タスク名を入力してください：");
+                    String task = sc.next();
+                    todoList.add(task);
+                    System.out.println("「" + task + "」を追加しました。");
+                    break;
+                case 2:
+                    System.out.println("===== タスク一覧 =====");
+                    for (int i = 0; i < todoList.size(); i++) {
+                        System.out.println((i + 1) + ". " + todoList.get(i));
+                    }
+                    break;
+                case 3:
+                    System.out.println("タスク削除を選択しました。");
+                    break;
+                case 0:
+                    System.out.println("アプリを終了します。");
+                    return;
+            }
         }
     }
 }
