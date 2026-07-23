3b4f323aeda59dc70ae4b229abb8c89ef36659a5
Yota Yamamoto <yota.06300630@gmail.com>
2026-07-23 20:30:49 +0900

    Version3.0 Todoクラス追加

---
+++ src/ToDo/Todo.java
@@ -0,0 +1,13 @@
+package ToDo;
+
+public class Todo {
+    private String task;
+
+    public Todo(String task){
+        this.task = task;
+    }
+
+    public String getTask() {
+        return task;
+    }
+}
--- src/ToDo/TodoService.java
+++ src/ToDo/TodoService.java
@@ -5,26 +5,27 @@ import java.util.ArrayList;
 //処理
 public class TodoService {
     //データ
-    private final ArrayList<String> todoList = new ArrayList<>();
+    private final ArrayList<Todo> todoList = new ArrayList<>();
 
     //追加
     public void addTask(String task) {
-        todoList.add(task);
+        Todo todo = new Todo(task);
+        todoList.add(todo);
     }
 
     //一覧表示
     public void showTask() {
         System.out.println("===== タスク一覧 =====");
         for (int i = 0; i < todoList.size(); i++) {
-            System.out.println((i + 1) + ". " + todoList.get(i));
+            System.out.println((i + 1) + ". " + todoList.get(i).getTask());
         }
     }
 
     //削除
     public void deleteTask(int deleteNum) {
         if (deleteNum >= 1 && deleteNum <= todoList.size()) {
-            String removedTask = todoList.remove(deleteNum - 1);
-            System.out.println("「" + removedTask + "」を削除しました。");
+            Todo removedTask = todoList.remove(deleteNum - 1);
+            System.out.println("「" + removedTask.getTask() + "」を削除しました。");
         } else {
             System.out.println("存在しない番号です。");
         }
